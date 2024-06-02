2fc6d76d8a124bd8083d776ce6eef7abb944e3e0
dmitrij.shymko <dmshimka@gmail.com>
2024-06-02 23:52:40 +0200

    Added appearance and disappearance animations.

--- app/src/main/java/com/example/animation/MainActivity.java
+++ app/src/main/java/com/example/animation/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.animation;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +11,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    boolean showTom = true;
+    ImageView tomImageView;
+    ImageView jerryImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,36 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        tomImageView = findViewById(R.id.tomCatImage);
+        jerryImageView = findViewById(R.id.jerryMouseImage);
+
+    }
+
+    public void animateImageDisappearance(ImageView imageView){
+        imageView.animate().alpha(0)
+                .rotation(imageView.getRotation() + 3600)
+                .scaleX(0.0F)
+                .scaleY(0.0F)
+                .setDuration(3000);
+    }
+    public void animateImageAppearance(ImageView imageView){
+        imageView.animate().alpha(1)
+                .rotation(imageView.getRotation() + 3600)
+                .scaleX(1F)
+                .scaleY(1F)
+                .setDuration(3000);
+    }
+
+    public void eraseTom(View view) {
+        if (showTom) {
+            animateImageDisappearance(tomImageView);
+            animateImageAppearance(jerryImageView);
+            showTom = false;
+        } else {
+            animateImageAppearance(tomImageView);
+            animateImageDisappearance(jerryImageView);
+            showTom = true;
+        }
     }
 }
